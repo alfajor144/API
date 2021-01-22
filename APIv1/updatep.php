da285de0ab30599fe0bc5f897cd0b71171e6a7f7
alfa jor 144 <karloxx09@gmail.com>
2021-01-22 03:13:25 -0300

    fixed put y delete

--- APIv1/updatep.php
+++ APIv1/updatep.php
@@ -1,8 +1,8 @@
 <?php
-
 header("Content-Type: application/json"); //header de las respuestas
 header('Access-Control-Allow-Origin: *'); //permite crusar informacion entre distintos dominos, muy importante! 
-header('Access-Control-Allow-Methods: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 include_once 'persona.php';
 
